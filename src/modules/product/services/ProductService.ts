7331973f7b099de8eaed2b586a5a8c7d7dc16272
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2022-07-03 18:06:56 -0300

    serviço para criação de um produto

---
+++ src/modules/product/services/ProductService.ts
@@ -0,0 +1,31 @@
+import AppError from '@shared/errors/AppError';
+import Product from '../entities/Product';
+import { ProductRepository } from './../repositories/ProductRepository';
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class ProductService {
+  public async create({ name, price, quantity }: IRequest): Promise<Product> {
+    const productExists = await ProductRepository.findByName(name);
+
+    if (productExists.length) {
+      throw new AppError('There is already one product with is name.');
+    }
+
+    const product = ProductRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await ProductRepository.save(product);
+
+    return product;
+  }
+}
+
+export default ProductService;
