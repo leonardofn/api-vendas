5b37db8addc505b55fff909d3d5be98ec705c795
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2022-12-17 10:21:19 -0300

    serviços de usuário: criar e listar

---
+++ src/modules/user/services/UserService.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+import User from '../entitities/User';
+import { UserRepository } from '../repositories/UserRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+  id?: string;
+}
+
+class UserService {
+  public async create({ name, email, password }: IRequest): Promise<User> {
+    const userExists = await UserRepository.findByEmail(email);
+
+    if (userExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const user = UserRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await UserRepository.save(user);
+
+    return user;
+  }
+
+  public async index(): Promise<User[]> {
+    const user = await UserRepository.find();
+
+    return user;
+  }
+}
+
+export default UserService;
