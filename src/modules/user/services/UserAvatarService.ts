ada49583ee6577ad422f55decf6957818811b425
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2022-12-17 22:10:06 -0300

    serviço para upload de arquivo para avatar

---
+++ src/modules/user/services/UserAvatarService.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+import fs from 'fs';
+import path from 'path';
+import uploadConfig from '../../../config/upload';
+import User from '../entities/User';
+import { UserRepository } from '../repositories/UserRepository';
+
+interface IRequest {
+  user_id: string;
+  avatarFileName: string;
+}
+
+class UserAvatarService {
+  public async uploadAvatar({
+    user_id,
+    avatarFileName,
+  }: IRequest): Promise<User> {
+    const user = await UserRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFileName;
+
+    await UserRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UserAvatarService;
