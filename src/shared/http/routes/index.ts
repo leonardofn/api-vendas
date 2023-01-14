593524f0124605ceba3b2e132b8b30870341ccd5
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-01-14 16:39:22 -0300

    Rota de Envio de Email - Forgot Password

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import productRouter from '@modules/product/routes/product.routes';
+import passwordRouter from '@modules/user/routes/password.routes';
 import userRouter from '@modules/user/routes/user.routes';
 import { Router } from 'express';
 import userAuthRouter from '../../../modules/user/routes/auth.routes';
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/products', productRouter);
 routes.use('/users', userRouter);
 routes.use('/auth', userAuthRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
