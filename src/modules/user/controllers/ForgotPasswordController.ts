43e33961d2563de1a1f330152754e4669902f2fb
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-01-14 16:12:48 -0300

    Controller de Tokens de Usuários - Forgot Password

---
+++ src/modules/user/controllers/ForgotPasswordController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  private sendForgotPasswordEmailService: SendForgotPasswordEmailService;
+
+  constructor() {
+    this.sendForgotPasswordEmailService = new SendForgotPasswordEmailService();
+  }
+
+  public create = async (
+    request: Request,
+    response: Response,
+  ): Promise<Response> => {
+    const { email } = request.body;
+
+    this.sendForgotPasswordEmailService.execute({ email });
+
+    return response.status(204).json();
+  };
+}
