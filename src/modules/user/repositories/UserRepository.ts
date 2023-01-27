69a62dfa895abf1df51067fdbf025dcdfca12c01
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-01-27 20:50:50 -0300

    ocultando e recuperando senha do usuário das buscas

--- src/modules/user/repositories/UserRepository.ts
+++ src/modules/user/repositories/UserRepository.ts
@@ -11,12 +11,14 @@ export const UserRepository = AppDataSource.getRepository(User).extend({
   async findById(id: string): Promise<User | null> {
     return this.createQueryBuilder('user')
       .where('user.id = :id', { id })
+      .addSelect('user.password')
       .getOne();
   },
 
   async findByEmail(email: string): Promise<User | null> {
     return this.createQueryBuilder('user')
       .where('user.email = :email', { email })
+      .addSelect('user.password')
       .getOne();
   },
 });
