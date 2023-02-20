1728712c8ac6dfe87ea87ca7d3e8290912fee9ae
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-02-20 15:20:13 -0300

    serviço para criar pedido de compra

---
+++ src/modules/orders/services/OrderService.ts
@@ -0,0 +1,89 @@
+import { CustomerRepository } from '@modules/customers/repositories/CustomerRepository';
+import { OrdersRepository } from '@modules/orders/repositories/OrdersRepository';
+import { ProductRepository } from '@modules/product/repositories/ProductRepository';
+import AppError from '@shared/errors/AppError';
+import { StatusCodes } from 'http-status-codes';
+import Order from '../entities/Order';
+
+interface IProduct {
+  id: string;
+  quantity: number;
+}
+
+interface IRequest {
+  customer_id: string;
+  products: IProduct[];
+}
+
+class OrderService {
+  public async create({ customer_id, products }: IRequest): Promise<Order> {
+    const customerExists = await CustomerRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError(
+        'Could not find any customer with the given id.',
+        StatusCodes.NOT_FOUND,
+      );
+    }
+    const foundProducts = await ProductRepository.findAllByIds(products);
+
+    if (foundProducts.length === 0) {
+      throw new AppError(
+        'Could not find any products with the given ids.',
+        StatusCodes.NOT_FOUND,
+      );
+    }
+
+    const foundProductsIds = foundProducts.map(products => products.id);
+
+    const checkInexistentProducts = products.filter(
+      product => !foundProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length > 0) {
+      throw new AppError(
+        `Could not find any product ${checkInexistentProducts[0].id}.`,
+        StatusCodes.NOT_FOUND,
+      );
+    }
+
+    const quantityAvailable = products.filter(
+      prodOrder =>
+        foundProducts.filter(prodFound => prodFound.id === prodOrder.id)[0]
+          .quantity < prodOrder.quantity,
+    );
+
+    if (quantityAvailable.length > 0) {
+      throw new AppError(
+        `The quantity ${quantityAvailable[0].quantity} is not available for product ${quantityAvailable[0].id}`,
+        StatusCodes.NOT_FOUND,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: foundProducts.find(p => p.id === product.id)?.price ?? 0,
+    }));
+
+    const order = await OrdersRepository.createOrder({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const updatedProductQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        foundProducts.filter(p => p.id === product.id)[0].quantity -
+        product.quantity,
+    }));
+
+    await ProductRepository.save(updatedProductQuantity);
+
+    return order;
+  }
+}
+
+export default OrderService;
--- src/modules/product/repositories/ProductRepository.ts
+++ src/modules/product/repositories/ProductRepository.ts
@@ -1,10 +1,22 @@
 import { AppDataSource } from '@config/db.config';
 import Product from '../entities/Product';
 
+interface IProductId {
+  id: string;
+}
+
 export const ProductRepository = AppDataSource.getRepository(Product).extend({
   async findByName(name: string): Promise<Product | null> {
     return this.createQueryBuilder('product')
       .where('product.name = :name', { name })
       .getOne();
+  },
+
+  async findAllByIds(products: IProductId[]): Promise<Product[]> {
+    const productIds = products.map(product => product.id);
+
+    return this.createQueryBuilder('product')
+      .where('product.id IN (:...productIds)', { productIds })
+      .getMany();
   },
 });
