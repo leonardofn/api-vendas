f7ecc03bea2b530778908b553e894ad4471b4f17
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-02-20 15:34:13 -0300

    controller de pedidos de compras

---
+++ src/modules/orders/controllers/OrderController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from 'express';
+import OrderService from '../services/OrderService';
+
+export default class ProductsController {
+  private orderService: OrderService;
+
+  constructor() {
+    this.orderService = new OrderService();
+  }
+
+  public show = async (
+    request: Request,
+    response: Response,
+  ): Promise<Response> => {
+    const { id } = request.params;
+
+    const order = await this.orderService.show(id);
+
+    return response.json(order);
+  };
+
+  public create = async (
+    request: Request,
+    response: Response,
+  ): Promise<Response> => {
+    const { customer_id, products } = request.body;
+
+    const order = await this.orderService.create({ customer_id, products });
+
+    return response.json(order);
+  };
+}
