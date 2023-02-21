f22e84729d9f6bfa1ab0e87cf0972f18a16d3bec
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-02-21 16:09:06 -0300

    middleware rate limiter

---
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Redis from 'ioredis';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    const redisClient = new Redis({
+      host: process.env.REDIS_HOST,
+      port: Number(process.env.REDIS_PORT),
+      password: process.env.REDIS_PASS || undefined,
+    });
+
+    const limiter = new RateLimiterRedis({
+      storeClient: redisClient,
+      keyPrefix: 'ratelimit',
+      points: 5,
+      duration: 1,
+    });
+
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('Too many requests.', StatusCodes.TOO_MANY_REQUESTS);
+  }
+}
