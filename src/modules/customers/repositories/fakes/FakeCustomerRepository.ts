4b6eb130b950a1ed3483f25b155cc59efd2c1720
Leonardo Feitosa <leonardofeitosa3010@gmail.com>
2023-06-09 16:31:08 -0300

    criando testes para criação de um novo cliente

---
+++ src/modules/customers/repositories/fakes/FakeCustomerRepository.ts
@@ -0,0 +1,69 @@
+import Customer from '@modules/customers/entities/Customer';
+import { ICreateCustomer } from '@modules/customers/models/create-customer.model';
+import { ICustomerRepository } from '@modules/customers/models/customer-repository.model';
+import { ICustomer } from '@modules/customers/models/customer.model';
+import { v4 as uuidv4 } from 'uuid';
+
+class FakeCustomersRepository implements ICustomerRepository {
+  private customers: Customer[] = [];
+
+  public async create({ name, email }: ICreateCustomer): Promise<Customer> {
+    const customer = new Customer();
+
+    customer.id = uuidv4();
+    customer.name = name;
+    customer.email = email;
+
+    this.customers.push(customer);
+
+    return customer;
+  }
+
+  public async save(customer: Customer): Promise<Customer> {
+    const { id } = customer;
+    const customerIndex = this.customers.findIndex(
+      customer => customer.id === id
+    );
+
+    this.customers[customerIndex] = customer;
+
+    return customer;
+  }
+
+  public async findByName(name: string): Promise<Customer | null> {
+    const customer = this.customers.find(customer => customer.name === name);
+    return customer ?? null;
+  }
+
+  public async findByIdWithProducts(id: string): Promise<ICustomer | null> {
+    const customer = this.customers.find(customer => customer.id === id);
+    return customer ?? null;
+  }
+
+  public async findById(id: string): Promise<Customer | null> {
+    const customer = this.customers.find(customer => customer.id === id);
+    return customer ?? null;
+  }
+
+  public async findByEmail(email: string): Promise<Customer | null> {
+    const customer = this.customers.find(customer => customer.email === email);
+    return customer ?? null;
+  }
+
+  public async find(): Promise<ICustomer[]> {
+    return this.customers;
+  }
+
+  public async remove(customer: ICustomer): Promise<ICustomer> {
+    const { id } = customer;
+    const customerIndex = this.customers.findIndex(
+      customer => customer.id === id
+    );
+
+    this.customers.splice(customerIndex, 1);
+
+    return customer;
+  }
+}
+
+export default FakeCustomersRepository;
--- src/modules/customers/services/CustomerService.spec.ts
+++ src/modules/customers/services/CustomerService.spec.ts
@@ -1,3 +1,37 @@
-it('should to 10', () => {
-  expect(5 + 5).toBe(10);
+import 'reflect-metadata';
+import AppError from '@shared/errors/AppError';
+import FakeCustomersRepository from '../repositories/fakes/FakeCustomerRepository';
+import CustomerService from './CustomerService';
+
+let fakeCustomersRepository: FakeCustomersRepository;
+let customerService: CustomerService;
+
+describe('CustomerService', () => {
+  beforeEach(() => {
+    fakeCustomersRepository = new FakeCustomersRepository();
+    customerService = new CustomerService(fakeCustomersRepository);
+  });
+
+  it('should be able to create a new customer', async () => {
+    const customer = await customerService.create({
+      name: 'Leonardo Dev',
+      email: 'leonardodev@test.com',
+    });
+
+    expect(customer).toHaveProperty('id');
+  });
+
+  it('should not be able to create a two customer with the same email', async () => {
+    await customerService.create({
+      name: 'Leonardo Dev',
+      email: 'leonardodev@test.com',
+    });
+
+    const newCustomer = customerService.create({
+      name: 'Leonardo Dev',
+      email: 'leonardodev@test.com',
+    });
+
+    expect(newCustomer).rejects.toBeInstanceOf(AppError);
+  });
 });
